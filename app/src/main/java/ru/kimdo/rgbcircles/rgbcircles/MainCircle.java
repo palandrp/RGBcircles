fc4a014b21bbb2588f40f1d4f61807b6289a53e4
palandr <palandrp@gmail.com>
2017-04-20 22:56:32 +0500

    Создан класс MainCircle

---
+++ app/src/main/java/ru/kimdo/rgbcircles/rgbcircles/MainCircle.java
@@ -0,0 +1,30 @@
+package ru.kimdo.rgbcircles.rgbcircles;
+
+/**
+ * Created by kimdo on 20.04.17.
+ */
+public class MainCircle {
+
+    public static final int INIT_RADIUS = 50;
+    private int x;
+    private int y;
+    private int radius;
+
+    public MainCircle(int x, int y) {
+        this.x = x;
+        this.y = y;
+        radius = INIT_RADIUS;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
