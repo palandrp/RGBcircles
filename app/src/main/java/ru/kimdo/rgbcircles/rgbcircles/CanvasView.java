6dacdac7917c71a85985a18a1332a71c49e17263
palandr <palandrp@gmail.com>
2017-04-21 00:55:57 +0500

    Добавлена инициализация кисточки и главного круга.

--- app/src/main/java/ru/kimdo/rgbcircles/rgbcircles/CanvasView.java
+++ app/src/main/java/ru/kimdo/rgbcircles/rgbcircles/CanvasView.java
@@ -2,6 +2,7 @@ package ru.kimdo.rgbcircles.rgbcircles;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,9 +10,24 @@ import android.view.View;
  * Created by kimdo on 20.04.17.
  */
 public class CanvasView extends View {
+    private MainCircle mainCircle;
+    private Paint paint;
 
     public CanvasView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initMainCircle();
+        initPaint();
+        
+    }
+
+    private void initPaint() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+    }
+
+    private void initMainCircle() {
+        mainCircle = new MainCircle(200, 500)
     }
 
     @Override
