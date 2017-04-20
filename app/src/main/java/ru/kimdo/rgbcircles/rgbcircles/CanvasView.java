0cd97476f37151a9c6ee23ad40e23953f88bbe51
palandr <palandrp@gmail.com>
2017-04-20 22:42:14 +0500

    В классе CanvasView создан конструктор и переопределен метод onDraw

--- app/src/main/java/ru/kimdo/rgbcircles/rgbcircles/CanvasView.java
+++ app/src/main/java/ru/kimdo/rgbcircles/rgbcircles/CanvasView.java
@@ -1,7 +1,21 @@
 package ru.kimdo.rgbcircles.rgbcircles;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by kimdo on 20.04.17.
  */
-public class CanvasView  {
+public class CanvasView extends View {
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
 }
