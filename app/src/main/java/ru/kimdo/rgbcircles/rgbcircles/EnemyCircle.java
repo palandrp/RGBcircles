e1bb98b41d17e0e9e416ab37efd8d8a3bb0aa30c
palandr <palandrp@gmail.com>
2017-04-26 22:23:10 +0500

    Добавлена функция отражения кругов от границ игровой зоны

--- app/src/main/java/ru/kimdo/rgbcircles/rgbcircles/EnemyCircle.java
+++ app/src/main/java/ru/kimdo/rgbcircles/rgbcircles/EnemyCircle.java
@@ -53,5 +53,15 @@ public class EnemyCircle extends SimpleCircle {
     public void moveOneStep() {
         x += dx;
         y += dy;
+        checkBounds();
+    }
+
+    private void checkBounds() {
+        if (x > GameManager.getWidth() || x < 0) {
+            dx = -dx;
+        }
+        if (y > GameManager.getHeight() || y < 0) {
+            dy = -dy;
+        }
     }
 }
